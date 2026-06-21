e99decba46019962c9c093c3b34e662d7899c9f3
manalpatil13 <manalpatil03@gmail.com>
2026-06-21 18:24:05 +0530

    Update CORS for frontend deployment

--- backend/main.py
+++ backend/main.py
@@ -37,7 +37,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://meetingmind-ai-jade.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
